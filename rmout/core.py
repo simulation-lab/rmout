651bf169e4573176a3341a716f690625cd33fbc2
Mamoru Kinomura <kinomura.mamoru@gmail.com>
2020-10-27 22:05:57 +0900

    コメントの追加，不正な入力への対応

--- rmout/core.py
+++ rmout/core.py
@@ -12,6 +12,9 @@ def run():
 
 
 def rmout(debug=True):
+
+    # ゴミ箱送りファイルの拡張子リストを取得
+
     EXTENSION_FILE = '.rmoutrc'
     current_dir = os.getcwd()
     home_dir = pathlib.Path.home()
@@ -26,6 +29,8 @@ def rmout(debug=True):
         with open(extfile_in_curr, 'r') as f:
             target_ext_list += [e.strip() for e in f.readlines()]
     target_ext_set = set(target_ext_list)
+
+    # カレントディレクトリから拡張子リストに対応したファイルを抽出
 
     throwaway = []
     target_extension_code = 0
@@ -44,20 +49,36 @@ def rmout(debug=True):
             })
     print('a:  all files')
     print('x:  exit')
+
+    # input()からユーザー入力を取得し，ゴミ箱送り対象のコードリストを作成
+
     codes_string = input('send to trash [a]: ')
     codelist = codes_string.split(',')
     codelist = [code.strip().lower() for code in codelist]
+
+    # ゴミ箱送りの拡張子辞書を取得
+
     if 'a' in codelist or '' in codelist:
         pass
     elif 'x' in codelist:
         return None
     else:
+        # 不正な入力の処理
+        ext_code_list = [str(target['extension_code']) for target in throwaway]
+        invalid_codes = [code for code in codelist
+                         if not code in ext_code_list]
+        if invalid_codes:
+            raise TypeError(f'The input code is invalid. {invalid_codes}')
+
+        # ユーザー入力と一致した拡張子ファイルを抽出
         renew_throwaway = [target for target in throwaway
                            if str(target['extension_code']) in codelist]
         if renew_throwaway:
             throwaway = renew_throwaway
         else:
-            raise TypeError('入力したコードが不正です．')
+            raise TypeError('The input code is invalid.')
+
+    # 削除対象のファイルをゴミ箱へ送る
 
     if throwaway:
         for target in throwaway:
