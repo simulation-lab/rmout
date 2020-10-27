b4967ff90886b5ea5231fcd95694fb84b317a11b
Mamoru Kinomura <kinomura.mamoru@gmail.com>
2020-10-27 22:36:50 +0900

    コードを細分化

--- rmout/core.py
+++ rmout/core.py
@@ -6,85 +6,107 @@ from send2trash import send2trash
 
 @click.command()
 def run():
-    debug = False
+    debug = True
     rmout(debug)
     click.echo('finished.')
 
 
 def rmout(debug=True):
 
-    # ゴミ箱送りファイルの拡張子リストを取得
-
-    EXTENSION_FILE = '.rmoutrc'
     current_dir = os.getcwd()
-    home_dir = pathlib.Path.home()
-    extfile_in_curr = os.path.join(current_dir, EXTENSION_FILE)
-    extfile_in_home = os.path.join(home_dir, EXTENSION_FILE)
 
-    target_ext_list = []
-    if os.path.isfile(extfile_in_home):
-        with open(extfile_in_home, 'r') as f:
-            target_ext_list += [e.strip() for e in f.readlines()]
-    if os.path.isfile(extfile_in_curr):
-        with open(extfile_in_curr, 'r') as f:
-            target_ext_list += [e.strip() for e in f.readlines()]
-    target_ext_set = set(target_ext_list)
+    def get_extensions_set(current_dir) -> set:
+        # ゴミ箱送りファイルの拡張子リストを取得
 
-    # カレントディレクトリから拡張子リストに対応したファイルを抽出
+        EXTENSION_FILE = '.rmoutrc'
+        home_dir = pathlib.Path.home()
+        extfile_in_curr = os.path.join(current_dir, EXTENSION_FILE)
+        extfile_in_home = os.path.join(home_dir, EXTENSION_FILE)
 
-    throwaway = []
-    target_extension_code = 0
-    for extension in sorted(target_ext_set):
-        target_extension = '*' + extension
-        target_files = pathlib.Path(current_dir).glob(target_extension)
-        target_files = [f for f in target_files
-                        if os.path.isfile(os.path.join(current_dir, f))]
-        if target_files:
-            target_extension_code += 1
-            print(f'{target_extension_code}: {target_extension}  {len(target_files)}')
-            throwaway.append({
-                'extension_code': target_extension_code,
-                'extension': target_extension,
-                'file_path': target_files,
-            })
-    print('a:  all files')
-    print('x:  exit')
+        target_ext_list = []
+        if os.path.isfile(extfile_in_home):
+            with open(extfile_in_home, 'r') as f:
+                target_ext_list += [e.strip() for e in f.readlines()]
+        if os.path.isfile(extfile_in_curr):
+            with open(extfile_in_curr, 'r') as f:
+                target_ext_list += [e.strip() for e in f.readlines()]
+        target_ext_set = set(target_ext_list)
+        return target_ext_set
 
-    # input()からユーザー入力を取得し，ゴミ箱送り対象のコードリストを作成
+    target_ext_set = get_extensions_set(current_dir)
 
-    codes_string = input('send to trash [a]: ')
-    codelist = codes_string.split(',')
-    codelist = [code.strip().lower() for code in codelist]
+    def extract_files_by_extlist(target_ext_set):
+        # カレントディレクトリから拡張子リストに対応したファイルを抽出
 
-    # ゴミ箱送りの拡張子辞書を取得
+        throwaway = []
+        target_extension_code = 0
+        for extension in sorted(target_ext_set):
+            target_extension = '*' + extension
+            target_files = pathlib.Path(current_dir).glob(target_extension)
+            target_files = [f for f in target_files
+                            if os.path.isfile(os.path.join(current_dir, f))]
+            if target_files:
+                target_extension_code += 1
+                print(
+                    f'{target_extension_code}: {target_extension}  {len(target_files)}')
+                throwaway.append({
+                    'extension_code': target_extension_code,
+                    'extension': target_extension,
+                    'file_path': target_files,
+                })
+        print('a:  all files')
+        print('x:  exit')
+        return throwaway
 
-    if 'a' in codelist or '' in codelist:
-        pass
-    elif 'x' in codelist:
-        return None
-    else:
-        # 不正な入力の処理
-        ext_code_list = [str(target['extension_code']) for target in throwaway]
-        invalid_codes = [code for code in codelist
-                         if not code in ext_code_list]
-        if invalid_codes:
-            raise TypeError(f'The input code is invalid. {invalid_codes}')
+    throwaway = extract_files_by_extlist(target_ext_set)
 
-        # ユーザー入力と一致した拡張子ファイルを抽出
-        renew_throwaway = [target for target in throwaway
-                           if str(target['extension_code']) in codelist]
-        if renew_throwaway:
-            throwaway = renew_throwaway
+    def extract_target_from_userinput():
+        # input()からユーザー入力を取得し，ゴミ箱送り対象のコードリストを作成
+
+        codes_string = input('send to trash [a]: ')
+        codelist = codes_string.split(',')
+        codelist = [code.strip().lower() for code in codelist]
+        return codelist
+
+    codelist = extract_target_from_userinput()
+
+    def get_extensiondir_for_sendtotrash(codelist, throwaway):
+        # ゴミ箱送りの拡張子辞書を取得
+
+        if 'a' in codelist or '' in codelist:
+            pass
+        elif 'x' in codelist:
+            return None
         else:
-            raise TypeError('The input code is invalid.')
+            # 不正な入力の処理
+            ext_code_list = [str(target['extension_code'])
+                             for target in throwaway]
+            invalid_codes = [code for code in codelist
+                             if not code in ext_code_list]
+            if invalid_codes:
+                raise TypeError(f'The input code is invalid. {invalid_codes}')
 
-    # 削除対象のファイルをゴミ箱へ送る
+            # ユーザー入力と一致した拡張子ファイルを抽出
+            renew_throwaway = [target for target in throwaway
+                               if str(target['extension_code']) in codelist]
+            if renew_throwaway:
+                throwaway = renew_throwaway
+            else:
+                raise TypeError('The input code is invalid.')
+        return throwaway
 
-    if throwaway:
-        for target in throwaway:
-            for target_file_path in target['file_path']:
-                if debug:
-                    print(os.path.basename(target_file_path))
-                else:
-                    print(os.path.basename(target_file_path))
-                    send2trash(str(target_file_path))
+    throwaway = get_extensiondir_for_sendtotrash(codelist, throwaway)
+
+    def send_to_trash(throwaway):
+        # 削除対象のファイルをゴミ箱へ送る
+
+        if throwaway:
+            for target in throwaway:
+                for target_file_path in target['file_path']:
+                    if debug:
+                        print(os.path.basename(target_file_path))
+                    else:
+                        print(os.path.basename(target_file_path))
+                        send2trash(str(target_file_path))
+
+    send_to_trash(throwaway)
