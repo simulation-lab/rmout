1f004f3b984bd6183c54af62f52698d0cca1590f
Mamoru Kinomura <kinomura.mamoru@gmail.com>
2020-10-29 10:44:35 +0900

    変数名の変更

--- rmout/core.py
+++ rmout/core.py
@@ -22,7 +22,7 @@ def run():
 def rmout(debug=True):
     current_dir = os.getcwd()
     target_ext_set = get_extensions_set(current_dir)
-    throwaway_candidate = extract_files_by_extlist(target_ext_set, current_dir)
+    trash_candidate = extract_files_by_extlist(target_ext_set, current_dir)
     codelist = extract_target_from_userinput()
-    throwaway = get_extensiondir_for_sendtotrash(codelist, throwaway_candidate)
+    throwaway = get_extensiondir_for_sendtotrash(codelist, trash_candidate)
     send_to_trash(throwaway, debug)
