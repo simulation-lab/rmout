c7545c244b3d6ed98cdbe2f1640d972cc3d66436
Mamoru Kinomura <kinomura.mamoru@gmail.com>
2020-10-29 15:10:48 +0900

    テストコードの作成

---
+++ tests/test_core.py
@@ -0,0 +1,13 @@
+from click.testing import CliRunner
+from contextlib import contextmanager
+from rmout import core
+
+
+def test_no_args(mocker):
+    mocker.patch.object(core, 'rmout', return_value=None)
+    runner = CliRunner()
+    result = runner.invoke(core.run, [])
+    expected_code = 0
+    expected_output = 'finished.'
+    assert result.exit_code == expected_code
+    assert result.output == expected_output + '\n'
