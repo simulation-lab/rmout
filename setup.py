f9cfbb057b8a91f65a54db2e7a61e23ce3603527
Mamoru Kinomura <kinomura.mamoru@gmail.com>
2020-10-30 12:38:11 +0900

    バージョンの変更

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='rmout',
-    version='0.1.3',
+    version='0.1.4',
 
     author='kino',
     author_email='simulation.space.labs@gmail.com',
